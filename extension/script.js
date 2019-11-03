a95fdc23a1fe74421b684d5ffe7e5d09dc0cf142
Tax_Day <rohitkundu2014@gmail.com>
2019-11-03 02:18:38 -0500

    feat:integration with hardware

--- extension/script.js
+++ extension/script.js
@@ -1,14 +1,50 @@
 
+$(document).ready(function(){
+    $('#calibrate').click(function a()
+    {
+        fetch('http://172.16.249.255:8000/calibrate', {
+  method: 'post',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({a: 7, str: 'Some string: &=&'})
+
+});
+});
+});
+const interval = setInterval(function() {
+    var s = false;
+    fetch('http://172.16.249.255:8000/blur')
+    .then(response => response.text())
+    .then(data => {if(data == 'True') {s = true;} else {s = false;} console.log(s);
+
+    if(data =="True")
+        {blur();}
+        else
+        {unBlur();}
+});
+}, 2000);
+
+
+function blur()
+{
+    chrome.tabs.insertCSS({file: "blur.css"});
+
+}
+
+function unBlur()
+{
+    chrome.tabs.insertCSS({file:"unblur.css"})
+}/*
 $(document).ready(function() {
 $('#blur').click(function blur()
 {
-    chrome.tabs.insertCSS({file: "blur.css"});
 });
 });
 
 $(document).ready(function(){
     $('#unblur').click(function unBlur()
     {
-        chrome.tabs.insertCSS({file:"unblur.css"})
     });
 });
+*/
