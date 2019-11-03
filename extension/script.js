be3514020a016dfcd2a453d663372f8338cd52a8
Tax_Day <rohitkundu2014@gmail.com>
2019-11-02 23:19:10 -0400

    feat:fix extension

--- extension/script.js
+++ extension/script.js
@@ -1,1 +1,27 @@
-document.body.style.filter = "blur(10px)";
+
+$(document).ready(function() {
+$('#blur').click(function blur()
+{
+    chrome.tabs.insertCSS({file: "blur.css"});
+    //document.body.style.filter = "blur(10px)";
+    alert("FUCK"); 
+});
+});
+
+$(document).ready(function(){
+    $('#unblur').click(function unBlur()
+    {
+        chrome.tabs.insertCSS({file:"unblur.css"})
+    });
+});
+
+/*function blur()
+{
+    document.body.style.filter = "blur(10px)";
+}
+
+function unBlur()
+{
+    document.body.style.filter = "blur(0px)";
+}
+*/
