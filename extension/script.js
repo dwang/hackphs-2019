0b9e299e714cda4f90234d805f1531c1c161dd71
Tax_Day <rohitkundu2014@gmail.com>
2019-11-03 03:14:38 -0500

    fix: script.js

--- extension/script.js
+++ extension/script.js
@@ -5,39 +5,3 @@ $(document).ready(function(){
              );
     });
 });
-const interval = setInterval(function() {
-    var s = false;
-    fetch('http://172.16.249.255:8000/blur')
-    .then(response => response.text())
-    .then(data => {if(data == 'True') {s = true;} else {s = false;} console.log(s);
-
-    if(data =="True")
-        {blur();}
-        else
-        {unBlur();}
-});
-}, 2000);
-
-
-function blur()
-{
-    chrome.tabs.insertCSS({file: "blur.css"});
-
-}
-
-function unBlur()
-{
-    chrome.tabs.insertCSS({file:"unblur.css"})
-}/*
-$(document).ready(function() {
-$('#blur').click(function blur()
-{
-});
-});
-
-$(document).ready(function(){
-    $('#unblur').click(function unBlur()
-    {
-    });
-});
-*/
